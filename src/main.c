b9ead34090224767fc68add6b215f73645663637
isaiah0311 <isaiahl0311@gmail.com>
2024-11-11 16:35:27 -1000

    Add parsing command line arguments

--- src/main.c
+++ src/main.c
@@ -7,14 +7,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * Entry point for the program.
  * 
+ * \param[in] argc Number of command line arguments.
+ * \param[in] argv Array of command line arguments.
  * \return Exit code.
  */
-int main() {
-    FILE* in_file = fopen("res/test.txt", "rb");
+int main(int argc, char** argv) {
+    char* in_file_path = NULL;
+
+    for (int i = 1; i < argc; ++i) {
+        if (!strcmp(argv[i], "-i")) {
+            if (++i < argc) {
+                in_file_path = argv[i];
+            } else {
+                fprintf(stderr, "[ERROR] Missing input file after -i.\n");
+                return EXIT_FAILURE;
+            }
+        } else {
+            fprintf(stderr, "[WARN] Skipping invalid parameter: %s.\n",
+                argv[i]);
+        }
+    }
+
+    if (!in_file_path) {
+        fprintf(stderr, "[ERROR] No input file given.\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE* in_file = fopen(in_file_path, "rb");
 
     if (!in_file) {
         fprintf(stderr, "[ERROR] Failed to open input file.\n");
