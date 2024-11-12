a6672427d6178a1b07ee463db0debf3d7011ea03
isaiah0311 <isaiahl0311@gmail.com>
2024-11-11 15:15:18 -1000

    Add converting file data to bytes

--- src/main.c
+++ src/main.c
@@ -14,6 +14,50 @@
  * \return Exit code.
  */
 int main() {
-    printf("File to Array\n");
+    FILE* in_file = fopen("res/test.txt", "rb");
+
+    if (!in_file) {
+        fprintf(stderr, "[ERROR] Failed to open input file.\n");
+        return EXIT_FAILURE;
+    }
+
+    int result = fseek(in_file, 0, SEEK_END);
+    if (result) {
+        fprintf(stderr, "[ERROR] Failed to seek in file.\n");
+        return EXIT_FAILURE;
+    }
+
+    long in_file_size = ftell(in_file);
+    rewind(in_file);
+
+    printf("unsigned char data[] = {\n    ");
+
+    unsigned line_count = 0;
+    unsigned total_count = 0;
+
+    while (!feof(in_file)) {
+        unsigned char byte = 0;
+        
+        if (!fread(&byte, sizeof(byte), 1, in_file)) {
+            break;
+        }
+
+        printf("0x%.2X", (int) byte);
+        
+        if (++total_count == in_file_size) {
+            break;
+        }
+
+        if (++line_count == 12) {
+            printf(",\n    ");
+            line_count = 0;
+        } else {
+            printf(", ");
+        }
+    }
+
+    printf("\n};\n\nsize_t count = %u;\n", total_count);
+
+    fclose(in_file);
     return EXIT_SUCCESS;
 }
