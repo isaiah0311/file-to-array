fd4e4ff4e20a86bf08b4bb75d844b82597e0ff01
isaiah0311 <isaiahl0311@gmail.com>
2024-11-11 15:08:59 -1000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * \file main.c
+ * \author Isaiah Lateer
+ * 
+ * Converts a file to a byte array.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Entry point for the program.
+ * 
+ * \return Exit code.
+ */
+int main() {
+    printf("File to Array\n");
+    return EXIT_SUCCESS;
+}
